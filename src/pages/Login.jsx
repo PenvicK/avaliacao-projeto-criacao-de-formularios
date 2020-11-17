1b6ed0230093f72cd8ed448c2dfacf2e246008c3
[Vinicius Posser] <[viniciusposser37@gmail.com]>
2020-11-17 00:43:54 -0300

    page login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,10 +4,16 @@ import './styles/login.css';
 
 function Login(){
     return(
-        <>
-            
-            <h1>Login</h1>
-        </>
+        <div className="container-login">
+            <span id="text-login">login</span>
+            <input type="text" placeholder="Email" className="input-login"/>
+            <input type="password" placeholder="Senha" className="input-login" id="sen-login"/>
+            <a href="/esqueceu-a-senha" id="esq-sen">Esqueceu a senha?</a>
+            <div>
+                <a href="/entrar" className="btn-login">entrar</a>
+                <a href="/cadastro" className="btn-login" id="btn-login">Cadastrar</a>
+            </div>            
+        </div>
     );
 }
 export default Login;
